1085e3be51c9838888ba2fa21658836b242fae7f
John Goodliff <9030780+twocaretcat@users.noreply.github.com>
2026-04-12 22:32:23 -0600

    test: add tests for uppercase file extensions

--- tests/test-cases/shared/extensions.ts
+++ tests/test-cases/shared/extensions.ts
@@ -52,4 +52,48 @@ export const EXTENSIONS_TEST_CASE_MAP: Record<string, TestCase> = {
 			format: 'webp',
 		},
 	},
+	// A .PNG extension should result in a PNG screenshot
+	'.PNG extension': {
+		page,
+		screenshotConfig: {
+			outputPath: `${SHARED_OUTPUT_DIR}/${OUTPUT_FILENAME}-uppercase.PNG`,
+		},
+		expected: {
+			...expected,
+			format: 'png',
+		},
+	},
+	// A .JPG extension should result in a JPEG screenshot
+	'.JPG extension': {
+		page,
+		screenshotConfig: {
+			outputPath: `${SHARED_OUTPUT_DIR}/${OUTPUT_FILENAME}-uppercase.JPG`,
+		},
+		expected: {
+			...expected,
+			format: 'jpeg',
+		},
+	},
+	// A .JPEG extension should result in a JPEG screenshot
+	'.JPEG extension': {
+		page,
+		screenshotConfig: {
+			outputPath: `${SHARED_OUTPUT_DIR}/${OUTPUT_FILENAME}-uppercase.JPEG`,
+		},
+		expected: {
+			...expected,
+			format: 'jpeg',
+		},
+	},
+	// A .WEBP extension should result in a WebP screenshot
+	'.WEBP extension': {
+		page,
+		screenshotConfig: {
+			outputPath: `${SHARED_OUTPUT_DIR}/${OUTPUT_FILENAME}-uppercase.WEBP`,
+		},
+		expected: {
+			...expected,
+			format: 'webp',
+		},
+	},
 };
